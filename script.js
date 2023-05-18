2d91fc5a33cb96f042fe5583a0f45fea8d532856
Sangeeta Devi <sd705194@gmail.com>
2023-05-18 17:41:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var tipvalue=document.querySelector(".container3");
+function main()
+{
+    var input=document.getElementById("input").value;
+    var input1=document.getElementById("input1");
+    var tip=document.getElementById("range").value;
+    var span=document.querySelector(".span")
+    span.innerHTML=`${tip}%`;
+    var value=((tip)/100)*input;
+    tipvalue.innerHTML=`Tip Value = ${value}`;
+    var totalamount=parseInt(value)+parseInt(input);
+    input1.value=totalamount;
+}
+
