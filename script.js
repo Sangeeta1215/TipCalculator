e35c761bf9d0e775ef84dcb2f2ca43737f9d6b4a
Sangeeta Devi <sd705194@gmail.com>
2023-05-19 22:23:29 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var tipvalue=document.querySelector(".container3");
 function main()
 {
-    var input=document.getElementById("input").value;
+    var input=Number(document.getElementById("input").value);
     var input1=document.getElementById("input1");
     var tip=document.getElementById("range").value;
     var span=document.querySelector(".span")
     span.innerHTML=`${tip}%`;
-    var value=((tip)/100)*input;
+    var value=parseInt(((tip)/100)*input);
     tipvalue.innerHTML=`Tip Value = ${value}`;
-    var totalamount= Number(value+input);
+    var totalamount= Number((value)+(input));
     input1.value=totalamount;
 }
